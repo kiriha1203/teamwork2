e707936d70963d45e487b69368d4d9ad7356cbc0
takefumi mizumoto <mizumoto.takefumi@gmail.com>
2020-05-17 13:32:28 +0900

    Fix メール送信機能

--- app/mailers/delete_agenda_mailer.rb
+++ app/mailers/delete_agenda_mailer.rb
@@ -3,7 +3,7 @@ class DeleteAgendaMailer < ApplicationMailer
 
   def delete_agenda_mail(agenda)
     @agenda = agenda
-    team_member_email = Team.find_by(id: @agenda.team_id).users.pluck(:email)
-    mail to: team_member_email, subject: I18n.t('views.messages.delete_agenda')
+    @team_member_email = Team.find_by(id: @agenda.team_id).users.pluck(:email)
+    mail to: @team_member_email, subject: I18n.t('views.messages.delete_agenda')
   end
 end
