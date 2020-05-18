49eea6662331a44973721f01aeef0af865b2bd4d
takefumi mizumoto <mizumoto.takefumi@gmail.com>
2020-05-18 17:26:49 +0900

    Fix Userの呼び出し方を修正

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -39,10 +39,14 @@ class TeamsController < ApplicationController
   end
 
   def change_owner
-    @new_owner = User.find(params[:format])
-    @team.update_attributes(owner_id: @new_owner.id)
-    redirect_to @team, notice: I18n.t('views.messages.change_owner')
-    ChangeOwnerMailer.change_owner_mail(@new_owner, @team).deliver
+    @new_owner = Assign.find(params[:format])
+    unless current_user = @new_owner
+      @team.update_attributes(owner_id: @new_owner.id)
+      redirect_to @team, notice: I18n.t('views.messages.change_owner')
+      ChangeOwnerMailer.change_owner_mail(@new_owner, @team).deliver
+    else
+      redirect_to @team, notice: I18n.t('views.messages.not_change_owner')
+    end
   end
 
   def destroy
