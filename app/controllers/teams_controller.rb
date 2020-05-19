79a8837dd4e4fb741bcac7e0a3c8aa04bb5c153e
takefumi mizumoto <mizumoto.takefumi@gmail.com>
2020-05-19 19:06:16 +0900

    Fix mailを修正

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -44,8 +44,8 @@ class TeamsController < ApplicationController
       redirect_to @team, notice: I18n.t('views.messages.not_change_owner')
     else
       @team.update_attributes(owner_id: @new_owner.id)
-      redirect_to @team, notice: I18n.t('views.messages.change_owner')
       ChangeOwnerMailer.change_owner_mail(@new_owner, @team).deliver
+      redirect_to @team, notice: I18n.t('views.messages.change_owner')
     end
   end
 
--- app/mailers/change_owner_mailer.rb
+++ app/mailers/change_owner_mailer.rb
@@ -1,7 +1,9 @@
 class ChangeOwnerMailer < ApplicationMailer
-  def change_owner_mail(new_owner,team)
+  default from: 'from@example.com'
+
+  def change_owner_mail(new_owner, team)
     @new_owner = new_owner
     @team = team
-    mail to: @new_owner.email, subject: I18n.t('views.messages.change_owner')
+    mail to: @new_owner.user.email, subject: I18n.t('views.messages.change_owner')
   end
 end
